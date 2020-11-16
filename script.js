df818fa2321d0d2eebd2c05fad64e56db50619df
Kye Browning <kye.browning@gmail.com>
2020-11-16 09:34:38 -0600

    added random planet function

--- script.js
+++ script.js
@@ -1,22 +1,7 @@
 // Write your JavaScript code here!
 
 window.addEventListener("load", function() {
-   fetch("https://handlers.education.launchcode.org/static/planets.json").then(function(response){
-      response.json().then(function(json){
-         const missionTarget = document.getElementById("missionTarget");
-         missionTarget.innerHTML = `
-         <h2>Mission Destination</h2>
-            <ol>
-               <li>Name: ${json[0].name}</li>
-               <li>Diameter: ${json[0].diameter}</li>
-               <li>Star: ${json[0].star}</li>
-               <li>Distance from Earth: ${json[0].distance}</li>
-               <li>Number of Moons: ${json[0].moons}</li>
-            </ol>
-            <img src="${json[0].image}">
-         `
-      });      
-   });
+   
 
    let form = document.querySelector("form");
    let faultyItems = document.getElementById("faultyItems");
@@ -65,15 +50,23 @@ window.addEventListener("load", function() {
      }
    });
 
+   fetch("https://handlers.education.launchcode.org/static/planets.json").then(function(response){
+      response.json().then(function(json){
+         const missionTarget = document.getElementById("missionTarget");
+         let randomNum = Math.floor(Math.random()*json.length) + 1;
+         missionTarget.innerHTML = `
+         <h2>Mission Destination</h2>
+            <ol>
+               <li>Name: ${json[randomNum].name}</li>
+               <li>Diameter: ${json[randomNum].diameter}</li>
+               <li>Star: ${json[randomNum].star}</li>
+               <li>Distance from Earth: ${json[randomNum].distance}</li>
+               <li>Number of Moons: ${json[randomNum].moons}</li>
+            </ol>
+            <img src="${json[randomNum].image}">
+         `
+      });      
+   });
+
 });
-/* This block of code shows how to format the HTML once you fetch some planetary JSON!
-<h2>Mission Destination</h2>
-<ol>
-   <li>Name: ${}</li>
-   <li>Diameter: ${}</li>
-   <li>Star: ${}</li>
-   <li>Distance from Earth: ${}</li>
-   <li>Number of Moons: ${}</li>
-</ol>
-<img src="${}">
-*/
+
