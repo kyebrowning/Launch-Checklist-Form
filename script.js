51c2a28cced4096dc3a3a9a7d9611625c05d5dfa
Kye Browning <kye.browning@gmail.com>
2020-11-16 09:21:49 -0600

    added mission destination fetch data

--- script.js
+++ script.js
@@ -1,6 +1,23 @@
 // Write your JavaScript code here!
 
 window.addEventListener("load", function() {
+   fetch("https://handlers.education.launchcode.org/static/planets.json").then(function(response){
+      response.json().then(function(json){
+         const missionTarget = document.getElementById("missionTarget");
+         missionTarget.innerHTML = `
+         <h2>Mission Destination</h2>
+            <ol>
+               <li>Name: ${json[0].name}</li>
+               <li>Diameter: ${json[0].diameter}</li>
+               <li>Star: ${json[0].star}</li>
+               <li>Distance from Earth: ${json[0].distance}</li>
+               <li>Number of Moons: ${json[0].moons}</li>
+            </ol>
+            <img src="${json[0].image}">
+         `
+      });      
+   });
+
    let form = document.querySelector("form");
    let faultyItems = document.getElementById("faultyItems");
    let pilotStatus = document.getElementById("pilotStatus");
